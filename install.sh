04b5c6f0c248202c7f79db1c7904c80808df8f46
JoysgitHub <114746470+JoysgitHub@users.noreply.github.com>
2024-06-06 18:29:18 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,13 @@ sudo chod +x mash
 
 sudo cp loca /usr/bin
 sudo cp mash /usr/bin
+
+
+gcc cr.c -o cr
+gcc jc.c -o jc
+
+sudo chmod +x cr
+sudo chod +x jc
+
+sudo cp cr  /usr/bin
+sudo cp jc /usr/bin
