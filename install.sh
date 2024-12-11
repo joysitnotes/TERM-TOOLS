0d531123f03b8f6fbd6b03e63c56aad2bdc6ae46
JoysgitHub <114746470+JoysgitHub@users.noreply.github.com>
2024-12-11 21:15:09 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ sudo chmod +x mash
 sudo chmod +x ipl
 sudo chmod +x update
 sudo chmod +x web
+sudo chmod +x rc
 gcc cr.c -o cr
 gcc jc.c -o jc
 
@@ -10,10 +11,20 @@ sudo chmod +x cr
 sudo chmod +x jc
 
 sudo cp loca /usr/bin
+echo "loca Installed"
 sudo cp mash /usr/bin
+echo "msh Installed"
 sudo cp ipl /usr/bin
+echo "ipl Installed"
 sudo cp update /usr/bin
+echo "update Installed"
 sudo cp web /usr/bin
+echo "web Installed"
 sudo cp cr  /usr/bin
+echo "cr Installed"
 sudo cp jc /usr/bin
+echo "jc Installed"
 sudo cp /usr/bin/clear /usr/bin/cls
+sudo cp rc /usr/bin
+echo "rc Installed"
+
