2c137b40b92b8fd360e0d97cef1857f14bb1447a
joysitnotes <114746470+joysitnotes@users.noreply.github.com>
2025-09-06 01:33:08 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,28 +17,50 @@ sudo chmod +x jc
 
 
 sudo cp loca /usr/bin
-echo "loca Installed"
+echo "[+] loca Installed"
 sudo cp mash /usr/bin
-echo "msh Installed"
+echo "[+] mash Installed"
 sudo cp ipl /usr/bin
-echo "ipl Installed"
+echo "[+] ipl Installed"
 sudo cp update /usr/bin
-echo "update Installed"
+echo "[+] update Installed"
 sudo cp web /usr/bin
-echo "web Installed"
-sudo cp cr  /usr/bin
-echo "cr Installed"
+echo "[+] web Installed"
+sudo cp cr /usr/bin
+echo "[+] cr Installed"
 sudo cp jc /usr/bin
-echo "jc Installed"
+echo "[+] jc Installed"
 sudo cp li /usr/bin
-
+echo "[+] li Installed"
 echo "li Installed"
 sudo cp /usr/bin/clear /usr/bin/cls
 sudo cp rc /usr/bin
 echo "rc Installed"
 echo "rc Installed"
 
-sudo apt install locate neovim -y && sudo updatedb
 
-mkdir $HOME/.config/nvim/
-touch $HOME/.config/nvim/init.vim
+read -p "[*] Do you want to install Redshift? (y/n)" answer
+
+if [[ "$answer" == "y" || "$answer" == "Y"  ]]; then
+  sudo apt install redshift -y
+  sudo chmod +x red
+  sudo chmod +x blu
+  sudo cp red /usr/bin
+  sudo cp blu /usr/bin
+  echo "[+] Use red to enable night light and blu to diable"
+else
+  echo "[+] Skipping Redshift..."
+fi
+
+
+read -p "[*] Do you want to install Neovim? (y/n)" answer
+
+if [[ "$answer" == "y" || "$answer" == "Y"  ]]; then
+  sudo apt install neovim -y
+  mkdir $HOME/.config/nvim/
+  touch $HOME/.config/nvim/init.vim
+else
+  echo "[+] Skipping Neovim..."
+fi
+echo 
+sudo apt install locate -y && sudo updatedb
